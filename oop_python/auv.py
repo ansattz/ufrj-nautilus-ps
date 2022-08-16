4477ea3f9d6eefe4199080a625e5dcee510ca2b2
ansattz <dvieiras@aol.com>
2022-08-16 03:19:40 -0300

    oop python (tables)

--- oop_python/auv.py
+++ oop_python/auv.py
@@ -1,3 +1,6 @@
+import numpy as np
+from tabulate import tabulate as tab
+
 class Vehicle:
     def __init__(self, name, thruster, sensor, birth, hydrophones):
         self.name=name
@@ -8,53 +11,52 @@ class Vehicle:
 
     def infoAUV(self):
         ''' this will define my row(the auv info) for the table '''
-        return [self.name, self.thruster, self.sensor, self.birth,
-        self.hydrophones]
-
-    def showThisAUV(self):
-        return self.name, self.thruster, self.sensor, self.birth, self.hydrophones
+        return np.array([self.name, self.thruster, self.sensor, self.birth,
+        self.hydrophones])
     
     def birthInfo(self):
-        return [self.birth,[self.name, self.thruster, self.sensor, self.birth,
-        self.hydrophones]]
+        return self.birth 
     
     def sensorsAUV(self):
-        return [self.name, self.sensor]
+        return np.array([self.name, self.sensor])
 
-v1 = Vehicle("Lua",8 ,["SENSOR X1", "SENSOR X2"], 2022,4)
-v2 = Vehicle("BrHUE",6,["SENSOR Y1", "SENSOR Y2"], 2021,4)
+v1 = Vehicle("Lua",8 ,"SENSOR X1, SENSOR X2", 2022,4)
+v2 = Vehicle("BrHUE",6,"SENSOR Y1, SENSOR Y2", 2021,4)
 
-AUVs = [v1,v2]
-numAUVs= range(0,len(AUVs))
+AUVs1 = [v1,v2]
+
+AUVs = [v1.infoAUV(), v2.infoAUV()]
 
 def tableOfAUVs():
     ''' return a table with all auvs '''
-    AUVsTable=[]
-    for i in numAUVs:
-        AUVsTable.append(AUVs[i].infoAUV())
-    return AUVsTable
+    tHeader = ["Name", "Thruster", "Sensors", "Birth", "Hydrophones"]
+    return tab(AUVs, headers=tHeader, tablefmt="fancy_grid")
 
-def anAUV(x=0):
-    ''' default because '''
-    if x < len(AUVs):
-        return AUVs[x].showThisAUV()
-    else:
-        return str.format('Invalid index. The max. index that you can choose is {}', len(AUVs) - 1)
+def anAUV():
+    ''' return a row table of an auv ''' 
+    tHeader = ["Name", "Sensor", "Thruster", "Birth", "Hydrophones"]
+    return tab([v2.infoAUV()], headers=tHeader, tablefmt="fancy_grid")
 
 def newer():
-    sortedBirth=[]
-    for i in numAUVs:
-        sortedBirth.append(AUVs[i].birthInfo())
-        sortedBirth.sort()
-    for i in numAUVs:
-        del sortedBirth[i][0]
-    return sortedBirth[::-1]
+    tHeader = ["Name", "Sensor", "Thruster", "Birth", "Hydrophones"]
+    l =[]
+    sortedB=[]
+    for i in range(0, len(AUVs1)):
+        l.append(str(AUVs1[i].birthInfo()))
+    for i in range(0, len(l)):
+        if max(l) in AUVs1[i].infoAUV():
+           sortedB.append(AUVs1[i].infoAUV())
+           l.remove(max(l))
+    if len(l) > 0:
+        for i in range(0,len(l)):
+            if max(l) in AUVs1[i].infoAUV():
+                sortedB.append(AUVs1[i].infoAUV())
+                l.remove(max(l))
+    return tab(sortedB, headers=tHeader, tablefmt="fancy_grid") 
 
 def AUVsensor():
     sensors=[]
-    for i in numAUVs:
-        sensors.append(AUVs[i].sensorsAUV())
-    return sensors
-print(AUVsensor())
-
-
+    tHeader=["Name", "Sensor"]
+    for i in range(0,len(AUVs1)):
+        sensors.append(AUVs1[i].sensorsAUV())
+    return tab(sensors,headers=tHeader, tablefmt="fancy_grid")
